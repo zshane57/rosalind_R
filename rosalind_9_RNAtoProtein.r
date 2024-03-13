7e2cfcd8373d12221462f17db980551051e64add
zshane57 <58837807+zshane57@users.noreply.github.com>
2024-03-14 02:02:41 +0800

    Update rosalind_9_RNAtoProtein.r

--- rosalind_9_RNAtoProtein.r
+++ rosalind_9_RNAtoProtein.r
@@ -1,13 +1,10 @@
 library("Biostrings")
 
-#read seq files
 fileName <- "D:/Download/rosalind_prot.txt"
 rnafile <- readLines(fileName)
 
-#read file as RNA
 rna <- RNAString(rnafile)
 
-#translate RNA to protein
 protein <- translate(rna)
 protein <- as.character(protein)
 protein <- str_replace_all(protein, "\\*", "")
