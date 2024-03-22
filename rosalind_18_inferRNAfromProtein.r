1cda9518e3ba0c54dcf7373f0424ea21311edc23
zshane57 <58837807+zshane57@users.noreply.github.com>
2024-03-23 00:15:48 +0800

    Add files via upload

---
+++ rosalind_18_inferRNAfromProtein.r
@@ -0,0 +1,29 @@
+library(gmp)
+
+codons <- c("F"  = 2, "L"  = 6, "S"  = 6, "Y"  = 2, "*"  = 3, 
+            "C"  = 2, "W"  = 1, "P"  = 4, "H"  = 2, "Q"  = 2, 
+            "R"  = 6, "V"  = 4, "A"  = 4, "D"  = 2, "E"  = 2, 
+            "G"  = 4, "I"  = 3, "M"  = 1, "T"  = 4, "N"  = 2, "K"  = 2)
+
+fileName <- "D:/Download/rosalind_mrna.txt"
+aaseq <- readLines(fileName)
+aaseq <- as.character(unlist(strsplit(aaseq, split="")))
+
+combi <- as.bigz(0)
+
+for(i in aaseq){
+    for(j in names(codons)){
+        if(combi == 0 && i == j){
+            combi <- combi + as.numeric(codons[j])
+        }
+        if(i == j){
+            combi <- combi * as.numeric(codons[j])
+        }
+    }
+}
+
+combi <- combi * as.numeric(codons["*"]) 
+modulo <- combi %% 1000000
+
+cat("Combination:\n", as.character(combi), "\n")
+cat("Modulus:\n", as.character(modulo), "\n")
