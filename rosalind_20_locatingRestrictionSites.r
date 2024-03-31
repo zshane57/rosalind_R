122c6654a75822cb2b0d711cb1f1cf14ce231572
zshane57 <58837807+zshane57@users.noreply.github.com>
2024-03-31 17:29:12 +0800

    Add files via upload

---
+++ rosalind_20_locatingRestrictionSites.r
@@ -0,0 +1,36 @@
+library(Biostrings)
+dss2df <- function(dss) {
+  data.frame(
+    seq = as.character(dss)
+  )
+}
+
+fileName <- "D:/Download/rosalind_revp.txt"
+#fileName <- "D:/Download/sample.txt"
+fasta_set <- readDNAStringSet(fileName)
+seq <- as.character(fasta_set[[1]])
+len <- nchar(seq)
+
+palindrome <- list()
+
+for (i in 1:(len-3)){
+    for(j in 3:12){
+        if(i+j > len){
+            break
+        }
+        subseq1 <- substr(seq, i, i+j)
+        subseq2 <- as.character(reverseComplement(DNAString(subseq1)))
+
+        if (subseq1 == subseq2){
+            palindrome <- c(palindrome, list(c(i,nchar(subseq2))))
+            #cat("subseq1: ", subseq1, "\n")
+            #cat("comp: ", subseq2, "\n")
+            #cat(i, j, "\n")
+        }
+    }
+}
+
+for (i in palindrome){
+    cat(paste(i, collapse = " "), "\n")
+    cat(paste(i, collapse = " "), "\n", file = "D:/Download/out_revp1.txt", append = TRUE)
+}
