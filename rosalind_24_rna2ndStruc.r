de4a0b23939284cd8629262705bcde31ea80d4ff
zshane57 <58837807+zshane57@users.noreply.github.com>
2024-04-06 01:09:36 +0800

    Add files via upload

---
+++ rosalind_24_rna2ndStruc.r
@@ -0,0 +1,18 @@
+library(Biostrings)
+library(gmp)
+
+perfect_match <- function(rna){
+    cnt_AU <- as.bigz(str_count(rna, pattern = "A"))
+    cnt_GC <- as.bigz(str_count(rna, pattern = "G"))
+
+    match <- factorial(cnt_AU) * factorial(cnt_GC)
+
+    return(match)
+}
+
+fileName <- "D:/Download/rosalind_pmch.txt"
+#fileName <- "D:/Download/sample.txt"
+fasta_set <- readRNAStringSet(fileName)
+rna <- as.character(fasta_set[[1]])
+
+cat(format(perfect_match(rna), scientific = FALSE), "\n")
