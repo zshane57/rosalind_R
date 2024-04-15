d6ef2417cda1b334f5d9ef5b884d0e34ca7a6d67
zshane57 <58837807+zshane57@users.noreply.github.com>
2024-04-16 02:16:16 +0800

    Add files via upload

---
+++ rosalind_25_ORF.r
@@ -0,0 +1,58 @@
+library(Biostrings)
+library(stringr)
+library(dplyr)
+
+fileName <- "D:/Download/rosalind_orf.txt"
+#fileName <- "D:/Download/sample.txt"
+fasta_set <- readDNAStringSet(fileName)
+dna <- as.character(fasta_set[[1]])
+rc_dna <- reverseComplement(fasta_set)
+rc_dna <- as.character(rc_dna[[1]])
+
+find_orfs <- function(dna_sequence) {
+  start_codon <- "ATG"
+  stop_codons <- c("TAA", "TAG", "TGA")
+
+  orfs <- c()
+  seq_len <- nchar(dna_sequence)
+
+  for(i in 1:seq_len) {
+    codon <- substr(dna_sequence, i, i + 2)
+
+    if(codon == start_codon) {
+      for(j in seq(i, seq_len, by = 3)) {
+        next_codon <- substr(dna_sequence, j, j + 2)
+
+        if(next_codon %in% stop_codons) {
+          orf <- substr(dna_sequence, i, j + 2)
+          orfs <- c(orfs, orf)
+          break
+        }
+      }
+    }
+  }
+  return(orfs)
+}
+
+dna2aa <- function(seq) {
+  seq_set <- find_orfs(seq)
+  prot <- c()
+  for(i in seq_set){
+    aa <- translate(DNAString(i))
+    prot <- c(prot, sub("\\*", "" , as.character(aa)))
+  }
+  return(prot)
+}
+
+print_aa <- function(rc_seq, seqs) {
+  rc <- dna2aa(rc_seq)
+  ori <- dna2aa(seqs)
+
+  aa_set <- c(rc, ori)
+  aa_set <- unique(aa_set)
+  for(i in aa_set){
+    cat(i, "\n", file = "D:/Download/orf_out.txt", append = TRUE)
+  }
+}
+
+print_aa(rc_dna, dna)
