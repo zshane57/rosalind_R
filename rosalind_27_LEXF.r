42297bdc21265f358a8014a1c82ae177a9487eea
zshane57 <58837807+zshane57@users.noreply.github.com>
2024-04-24 22:56:27 +0800

    Update rosalind_27_LEXF.r

--- rosalind_27_LEXF.r
+++ rosalind_27_LEXF.r
@@ -1,11 +1,11 @@
 library(gtools)
 
-file_content <- readLines("D:/Download/rosalind_lexf.txt")
-#file_content <- readLines("D:/Download/sample.txt")
+fileName <- readLines("D:/Download/rosalind_lexf.txt")
+#fileName <- readLines("D:/Download/sample.txt")
 
-string <- unlist(strsplit(file_content[1], " "))
-n <- as.integer(file_content[2])
+string <- unlist(strsplit(fileName[1], " "))
+n <- as.integer(fileName[2])
 
 result <- permutations(n = length(string), r = n, v = string, repeats.allowed = TRUE)
 
-cat(apply(result, 1, paste, collapse = ""), sep = "\n", file = "D:/Download/out_lexf.txt")
+cat(apply(result, 1, paste, collapse = ""), sep = "\n")#, file = "D:/Download/out_lexf.txt")
